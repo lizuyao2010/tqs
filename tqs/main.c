913d1575095abdc8239da216a21012bda4d0f73f
李 祖尧 <lizuyao@Michael-Pauls-Air.local>
2013-03-16 22:24:28 +0800

    Initial Commit

---
+++ tqs/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  tqs
+//
+//  Created by 李 祖尧 on 13-3-16.
+//  Copyright (c) 2013年 李 祖尧. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
