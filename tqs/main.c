3b92152339afface498b14bc55553a2ad9563424
lizuyao2010 <zuyaoli2010@gmail.com>
2013-05-24 15:46:23 +0800

    change header
    change header to <stdio.h>

--- tqs/main.c
+++ tqs/main.c
@@ -1,18 +1,13 @@
-//
-//  main.c
-//  tqs
-//
-//  Created by 李 祖尧 on 13-3-16.
-//  Copyright (c) 2013年 李 祖尧. All rights reserved.
-//
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[])
+int main()
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    int v=7;
+    int num=0;
+    while (v!=0)
+    {
+        if(v%2==1)
+            num++;
+        v=v/2;
+    }
+    printf("%d",num);
 }
-
